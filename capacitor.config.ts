a99780d08beb5ac579760bfc4f50bebedd6a4527
jamirnasci <jamirnasci@gmail.com>
2025-02-23 23:30:43 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'image-text-reader',
+  webDir: 'dist'
+};
+
+export default config;
